b680160c71f277e5e43021e6623db839059dfedf
Siddharth Iyer <ssi@mit.edu>
2019-05-30 14:06:57 -0400

    AHb works with wavelet

--- wshfl.py
+++ wshfl.py
@@ -12,15 +12,18 @@ class WaveShuffling(sp.app.App):
 
   def _construct_kernel(self):
 
-    self.kernel = self.xp.zeros([1, self.sy, self.sz, 1, 1, 1, self.tk, self.tk])
-    vec = self.xp.zeros((self.tk, 1))
-    mask = self.xp.zeros([1, self.sy, self.sz, 1, 1, self.tf, 1, 1])
+    self.kernel = self.xp.zeros([1, self.sy, self.sz, 1, 1, 1, self.tk, self.tk]).astype(self.xp.complex64)
+    vec = self.xp.zeros((self.tk, 1)).astype(self.xp.complex64)
+    mask = self.xp.zeros([self.sy, self.sz, self.tf]).astype(self.xp.complex64)
 
-    U = sp.linop.MatMul(vec.shape, self.phi.squeeze())
+    phi = self.phi.squeeze()
+    if (len(phi.shape) == 1):
+      phi = self.xp.reshape(phi, (phi.size, 1))
+    U = sp.linop.MatMul(vec.shape, phi)
 
     for k in range(self.rdr.shape[0]):
       [ky, kz, ec] = self.rdr[k, :]
-      mask[0, ky, kz, 0, 0, ec, 0, 0] = 1
+      mask[ky, kz, ec] = 1
 
     flags = set() 
     for k in range(self.rdr.shape[0]):
@@ -29,14 +32,23 @@ class WaveShuffling(sp.app.App):
       if key in flags:
         continue
       flags.add(key)
-      mvec = mask[0, ky, kz, 0, 0, :, 0, 0].squeeze()
+      mvec = mask[ky, kz, :].squeeze()
 
       for t in range(self.tk):
         vec = vec * 0
         vec[t] = 1
         self.kernel[0, ky, kz, 0, 0, 0, t, :] = U.H(U(vec).squeeze() * mvec).squeeze()
 
-  #def _construct_AHb():
+  def _construct_AHb(self):
+    x = self.xp.zeros((self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1)).astype(self.xp.complex64)
+    PhiH = self.phi.squeeze().conj()
+    if (len(PhiH.shape) == 1):
+      PhiH = self.xp.reshape(PhiH, (PhiH.size, 1))
+    for k in range(self.rdr.shape[0]):
+      [ky, kz, ec] = self.rdr[k, :]
+      for t in range(self.tk):
+        x[:, ky, kz, :, 0, 0, t, 0] = self.tbl[:, :, k] * PhiH[ec, t]
+    self.AHb = self.W(self.E.H(self.R.H(self.Fx.H(self.Psf.H(self.Fyz.H(x))))))
 
   def _broadcast_check(self, x):
     if(len(x.shape) == self.max_dims):
@@ -59,7 +71,7 @@ class WaveShuffling(sp.app.App):
       self.mps = self.xp.array(self._broadcast_check(mps))
       self.psf = self.xp.array(self._broadcast_check(psf))
       self.phi = self.xp.array(self._broadcast_check(phi))
-      self.cps = cps # Caipi-Shuffling
+      self.cps = cps # Caipi-Shuffling flag.
       self.lmb = lmb # Lambda.
       self.mit = mit # Max-Iter
 
@@ -72,18 +84,23 @@ class WaveShuffling(sp.app.App):
       self.tf = self.phi.shape[5]
       self.tk = self.phi.shape[6]
 
-      self._construct_kernel()
-      #self._construct_AHb()
+      wavelet_axes = tuple([k for k in range(3) if self.mps.shape[k] > 1])
 
-      self.E   = sp.linop.Multiply([self.sx, self.sy, self.sz, 1, 1, 1, self.tk, 1], self.mps)
-      self.R   = sp.linop.Resize([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], \
-                                 [self.sx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1])
-      self.Fx  = sp.linop.FFT([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], axes=(0,))
-      self.Psf = sp.linop.Multiply([self.sx, self.sy, self.sz, self.nc, 1, 1, 1], self.psf)
-      self.Fyz = sp.linop.FFT([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], axes=(1, 2))
-      self.K   = sp.linop.Sum(     [self.wx, self.sy, self.sz, 1, 1, 1, self.tk, self.tk], axes=(6,)) * \
-                 sp.linop.Multiply([self.wx, self.sy, self.sz, 1, 1, 1, self.tk,       1], self.kernel)
-      self.Wav = sp.linop.Wavelet([self.wx, self.sy, self.sz, 1, 1, 1, self.tk, 1], axes=(0, 1, 2))
+      assert(self.md == 1) # Until multiple ESPIRiT maps is implemented.
+
+      self._construct_kernel()
+
+      self.E      = sp.linop.Multiply([self.sx, self.sy, self.sz, 1, self.md, 1, self.tk, 1], self.mps)
+      self.R      = sp.linop.Resize([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], \
+                                  [self.sx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1])
+      self.Fx     = sp.linop.FFT([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], axes=(0,))
+      self.Psf    = sp.linop.Multiply([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], self.psf)
+      self.Fyz    = sp.linop.FFT([self.wx, self.sy, self.sz, self.nc, 1, 1, self.tk, 1], axes=(1, 2))
+      self.K      = sp.linop.Sum(     [self.wx, self.sy, self.sz, 1, 1, 1, self.tk, self.tk], axes=(6,)) * \
+                    sp.linop.Multiply([self.wx, self.sy, self.sz, 1, 1, 1, self.tk,       1], self.kernel)
+      self.W      = sp.linop.Wavelet([self.sx, self.sy, self.sz, 1, 1, 1, self.tk, 1], axes=wavelet_axes)
+
+      self._construct_AHb()
 
       # Only need to define grad f and prox g
       #self.AHA = E
